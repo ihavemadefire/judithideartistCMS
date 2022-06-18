2231425638df6246376200e355fa95f7f0fc15fc
Jacob <jacobmeide@gmail.com>
2022-06-18 13:50:45 -0500

    Initial wagtail instance

---
+++ judithideartistCMS/judithideartistCMS/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-n6$fx+a_c505(hf8x8er4a-w%u1)^56bz%a5)as%jxqg(qfy)e"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
